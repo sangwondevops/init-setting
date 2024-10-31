4ecaf7172573466905b7a3cdfc2fd46123fa2a4c
Koo Sangmo <60815812+rn0614@users.noreply.github.com>
2024-10-31 22:06:20 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd
+          Learn GithubAction cicd -test change1
         </a>
       </header>
     </div>
