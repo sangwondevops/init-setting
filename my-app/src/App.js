c13ca1b5d7343cc1b3c6ed68291d1011c23ab59d
Koo Sangmo <60815812+rn0614@users.noreply.github.com>
2024-11-10 19:59:51 +0900

    Update App.js
    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <h1>test source cicd2-3</h1>
       <h1>test source cicd2-4</h1>
       <h1>test source cicd2-5</h1>
+      <h1>test source cicd2-6</h1>
     </div>
   );
 }
